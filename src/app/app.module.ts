29be57505e8bf5feca14b46cd34e003cf324dd42
hulkkk <wfoskk@gmail.com>
2020-03-10 01:35:57 +0900

    親からDOMを取得する処理を追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,22 @@ import { AppComponent } from './app.component';
 import { IgxNavbarModule, IgxGridModule, IgxDatePickerModule } from 'igniteui-angular';
 import { IgniteGridComponent } from './grid/ignite-grid/ignite-grid.component';
 import { ParentGridComponent } from './grid/parent-grid/parent-grid.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './parent/child/child.component';
+import { Test1Component } from './parent/test1/test1.component';
+import { Test2Component } from './parent/test2/test2.component';
+import { Test3Component } from './parent/test3/test3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IgniteGridComponent,
     ParentGridComponent,
+    ParentComponent,
+    ChildComponent,
+    Test1Component,
+    Test2Component,
+    Test3Component,
   ],
   imports: [
     BrowserModule,
--- src/app/grid/ignite-grid/ignite-grid.component.ts
+++ src/app/grid/ignite-grid/ignite-grid.component.ts
@@ -10,56 +10,16 @@ export class IgniteGridComponent implements OnInit {
 
   @ViewChild('grid1', { read: IgxGridComponent })
   public grid: IgxGridComponent;
-  public localData = [{ 'test1': 'test', 'test2': 'test', 'test3': 'test', },{ 'test1': 'test', 'test2': 'test', 'test3': 'test', }]
+  // public localData = [{ 'test1': 'test', 'test2': 'test', 'test3': 'test', },{ 'test1': 'test', 'test2': 'test', 'test3': 'test', }]
   // public localData =[]
 
   ngOnInit(): void {
   }
   @ViewChild('test')
   private test2: ElementRef;
-  constructor(private renderer: Renderer2) {
+  constructor() {
   }
 
   onClickMe() {
-    let btn = this.renderer.createElement('button');
-    let text = this.renderer.createText('ボタン作成');
-
-    this.renderer.setAttribute(btn, 'name', 'btnName');
-    this.renderer.addClass(btn, 'className');
-    this.renderer.setStyle(btn, 'width', '150px');
-
-    this.renderer.appendChild(btn, text);
-    this.renderer.appendChild(this.test2.nativeElement, btn);
-
-    const me = this.renderer.parentNode(this.test2.nativeElement);
-
-    const me2 = this.renderer.parentNode(me);
-    const me3 = this.renderer.parentNode(me2);
-    const me4 = this.renderer.parentNode(me3);
-
-    console.log(this.renderer);
-    console.log(this.renderer.parentNode(this.test2.nativeElement));
-    console.log(this.test2.nativeElement.childNodes[0]);
-    console.log('me');
-    console.log(this.renderer.parentNode(me));
-    console.log(me.childNodes[0]);
-    console.log(me.childNodes[1]);
-    console.log('me2');
-    console.log(this.renderer.parentNode(me2));
-    console.log(me2.childNodes[0]);
-    console.log(me2.childNodes[1]);
-    console.log('me3');
-    console.log(this.renderer.parentNode(me3));
-    console.log(me3.childNodes[0]);
-    console.log(me3.childNodes[1]);
-    console.log('me4');
-    console.log(this.renderer.parentNode(me4));
-    console.log(me4.childNodes[0]);
-    console.log(me4.childNodes[1]);
-    console.log(me4);
-
-    this.renderer.listen(btn, 'click', (event) => {
-      this.renderer.removeChild(this.test2.nativeElement, btn);
-    });
   }
 }
